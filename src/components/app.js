87d31db8cca6a69acea4e11c26b8e3dd39c24b44
Benjamin Brenner <ben.l.brenner@gmail.com>
2017-01-27 12:53:37 -0800

    6.75

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Greeting } from '../routes'
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        {this.props.children}
+      </div>
     );
   }
 }
